e3be1155f38e22ee548ba1f96255d8f4bcc6c80d
nuptLMF <630944715@qq.com>
2019-06-19 00:15:01 +0800

    增加处理异步加载问题功能以及翻页功能，注释掉下载图片功能

--- selenium_pr.py
+++ selenium_pr.py
@@ -4,8 +4,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import requests
 import redis
 import json
+import os
+import pymongo
 from lxml import etree
 
 class JD_item(object):
@@ -17,7 +20,7 @@ class JD_item(object):
 		self.browser = webdriver.Chrome()
 
 	def login(self):
-		#初始化登陆，由于未添加滑块验证功能，这里需要进行手动滑块验证
+		#初始化登陆，由于极验验证破解功能，这里需要进行手动滑块。
 		self.browser.get('https://www.jd.com')
 		wait = WDW(self.browser,10)
 		self.browser.find_element_by_xpath('//*[@id="ttbar-login"]/a[1]').click()
@@ -35,13 +38,33 @@ class JD_item(object):
 		self.browser.close()
 
 	def save_to_redis(self,name,item_dic):
-		#将数据存储在redis数据库中（多维度数据大量爬取时建议使用sql或者mongodb数据库）
+		#将数据存储在redis数据库中（多维度数据大量爬取时建议使用sql或者mongodb数据库，本爬虫未调用）
 		try:
-			db2 = redis.StrictRedis(host = 'localhost',port=6379,db = 5,password = 'lvmingfu123')
+			db2 = redis.StrictRedis(host = 'localhost',port=6379,db = 5,password = '----')
 			db2.hset('JD:{}'.format(self.keywords),name,item_dic)
 			print('存储完成一个')
 		except:
-			print('数据存储出现错误')
+			print('redis数据存储出现错误')
+
+	def save_to_mongodb(self,item):
+		client = pymongo.MongoClient('localhost:27017')
+		db = client['JD']
+		db[self.keywords].update_one({'_id':item['_id']},{'$set':item},True)
+		print('mongodb数据存储成功',item)
+
+	def next_page(self,page_num):
+		wait = WDW(self.browser,10)
+		input = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="J_bottomPage"]/span[2]/input')))
+		input.clear()
+		input.send_keys(page_num)
+		input.send_keys(Keys.ENTER)
+#		total_pages = self.response.xpath('//*[@id="J_bottomPage"]/span[2]/em[1]/b/text()')[0]
+#		if page_num < int(total_pages):
+
+	def max_page(self):
+		#获取可以翻页的最大页数
+		total_pages = ''.join(self.response().xpath('//*[@id="J_bottomPage"]/span[2]/em[1]/b/text()'))
+		return int(total_pages)
 
 	def find_item(self):
 		#模拟在京东主页面搜索框进行搜索
@@ -51,33 +74,50 @@ class JD_item(object):
 			input.clear()
 			input.send_keys(self.keywords)
 			self.browser.find_element_by_xpath('//*[@id="search"]/div/div[2]/button').click()
-			self.parse_item()
 		except:
 			print('搜索框出现错误')
 
 	def response(self):
 		#将目前浏览的网页源代码转换成html
 		return etree.HTML(self.browser.page_source)
-	
+
+	def scroll_down(self):
+		#京东初始页面只有30个物品，需要处理异步加载问题，将页面滑至最底端进行加载
+		self.browser.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+		time.sleep(1)
+
 	def parse_item(self):
 		#爬取的物品的名称，价格，物品的主页面网址
+		count = 0	#计数页面总共有爬取下来的条目数量
+		self.scroll_down()
 		res = self.response()
-		try:
-			items = res.xpath('//*[@id="J_goodsList"]/ul/li')
-			item_dic = {}
-			for i in range(1,len(items)+1):
-				name = ''.join(res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/div/div[contains(@class,"-name")]//em/text()'.format(i))).replace(' ','')
-				price = ''.join(res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/div/div[contains(@class,"-price")]/strong//text()'.format(i)))
-				item_url = res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/div/div[contains(@class,"-name")]/a/@href'.format(i))[0]
-				if 'https:' not in item_url:
-					item_url = 'https:'+item_url
-				item_dic = {
-				'item_url':item_url,
-				'price':price
-				}
-				self.save_to_redis(name,json.dumps(item_dic))
-		except:
-			print('物品爬取错误!')
+		items = res.xpath('//*[@id="J_goodsList"]/ul/li')
+		item_dic = {}
+		for i in range(1,len(items)+1):
+			_id = ''.join(res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/@data-sku'.format(i)))
+			name = ''.join(res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/div/div[contains(@class,"-name")]//em/text()'.format(i))).replace(' ','').replace('\\','').replace('/','')
+			price = ''.join(res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/div/div[contains(@class,"-price")]/strong/i/text()'.format(i)))
+#			img = ''.join(res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/div/div[contains(@class,"-img")]/a//@src'.format(i)))
+#			if 'http:' not in img:
+#				img = 'http:'+img
+#			con = requests.get(img).content
+#			with open('E:\JD\{}\{}'.format(self.keywords,name)+'.jpg','wb') as f:
+#				f.write(con)
+			item_url = res.xpath('//*[@id="J_goodsList"]/ul/li[{}]/div/div[contains(@class,"-name")]/a/@href'.format(i))[0]
+			if 'https:' not in item_url:
+				item_url = 'https:'+item_url
+			item_dic = {
+			'_id':_id,
+			'item_url':item_url,
+			'name':name,
+#			'img':img,
+			'price':price
+			}
+			self.save_to_mongodb(item_dic)
+			count = count+1
+			print('本页爬取商品信息数量{}份'.format(count))
+
+		
 
 def main():
 	jd_account = '----' #----填入京东账号
@@ -86,8 +126,11 @@ def main():
 	JD = JD_item(jd_account,jd_pwd,item_word) 
 	JD.login()
 	JD.find_item()
-	time.sleep(5)
-	JD.parse_item()
+	time.sleep(3)
+	JD.scroll_down()
+	for i in range(1,JD.max_page()+1):
+		JD.parse_item()
+		JD.next_page(i)
 	JD.close()
 
 if __name__=='__main__':
